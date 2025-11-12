525d91653455e86954ca27ce3c6a06cf5036d425
Gabriele Lodi <gabriele.lodi93@gmail.com>
2025-11-12 12:04:34 +0100

    Remove par_conv_int8 folder test

--- opentitan-cluster/par_conv_int8/stimuli/conv.h
+++
@@ -1,6 +0,0 @@
-#include <stdio.h>
-
-void pulp_conv_int8_int8_int8(const int8_t *__restrict__ in1,
-                             const int8_t *__restrict__ in2,
-                             int8_t *__restrict__       out,
-                             int                      size);
