5304fea6adec6f6b2287bc35993f6d725c7f4863
Gabriele Lodi <gabriele.lodi93@gmail.com>
2025-11-12 14:33:13 +0100

    Working on printf for hello.c

--- hello/test.c
+++ hello/test.c
@@ -15,12 +15,48 @@
  */
 
 #include <stdio.h>
+#include <stdlib.h>
+#include "pulp.h"
+
+#define SHARED_ADDR 0xB0000000
+
+
+#define UART_BASE_ADDR 0x1A222000
+
+#define UART_REG_RBR ( UART_BASE_ADDR + 0x00) // Receiver Buffer Register (Read Only)
+#define UART_REG_DLL ( UART_BASE_ADDR + 0x00) // Divisor Latch (LS)
+#define UART_REG_THR ( UART_BASE_ADDR + 0x00) // Transmitter Holding Register (Write Only)
+#define UART_REG_DLM ( UART_BASE_ADDR + 0x04) // Divisor Latch (MS)
+#define UART_REG_IER ( UART_BASE_ADDR + 0x04) // Interrupt Enable Register
+#define UART_REG_IIR ( UART_BASE_ADDR + 0x08) // Interrupt Identity Register (Read Only)
+#define UART_REG_FCR ( UART_BASE_ADDR + 0x08) // FIFO Control Register (Write Only)
+#define UART_REG_LCR ( UART_BASE_ADDR + 0x0C) // Line Control Register
+#define UART_REG_MCR ( UART_BASE_ADDR + 0x10) // MODEM Control Register
+#define UART_REG_LSR ( UART_BASE_ADDR + 0x14) // Line Status Register
+#define UART_REG_MSR ( UART_BASE_ADDR + 0x18) // MODEM Status Register
+#define UART_REG_SCR ( UART_BASE_ADDR + 0x1C) // Scratch Register
+
+
+void uart_set_cfg(int parity, uint16_t clk_counter) {
+  unsigned int i;
+  *(volatile unsigned int*)(UART_REG_LCR) = 0x83; //sets 8N1 and set DLAB to 1
+  *(volatile unsigned int*)(UART_REG_DLM) = (clk_counter >> 8) & 0xFF;
+  *(volatile unsigned int*)(UART_REG_DLL) =  clk_counter       & 0xFF;
+  *(volatile unsigned int*)(UART_REG_FCR) = 0xA7; //enables 16byte FIFO and clear FIFOs
+  *(volatile unsigned int*)(UART_REG_LCR) = 0x03; //sets 8N1 and set DLAB to 0
+
+  *(volatile unsigned int*)(UART_REG_IER) = ((*(volatile unsigned int*)(UART_REG_IER)) & 0xF0) | 0x02; // set IER (interrupt enable register) on UART
+}
 
 int main()
 {
-  printf("Hello !\n");
+  int baud_rate = 115200;
+  int test_freq = 100000000;
+  uart_set_cfg(0,(test_freq/baud_rate)>>4);
 
-  *(int*)(0xB0000000)=0xABBAABBA;
+  printf("Prego, prenda pure una cadrega!\n\r");
+
+  *(int*)(SHARED_ADDR)=0xABBAABBA;
 
   return 0;
 }
