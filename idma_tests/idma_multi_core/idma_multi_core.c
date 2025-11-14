17becf42004f5fce5b49b81059ec2e37a5fb84f8
Gabriele Lodi <gabriele.lodi93@gmail.com>
2025-11-14 17:28:46 +0100

    Add write to mbox and some printf to idma_multi_core/2d/3d and matrixmul8/16 tests

--- idma_tests/idma_multi_core/idma_multi_core.c
+++ idma_tests/idma_multi_core/idma_multi_core.c
@@ -55,7 +55,7 @@ int test_idma_1D (int core_id, uint32_t size, int ext2loc, int loc2loc) {
     // Check the results
 
     for (int i=0; i < size; i++) {
-        uint8_t expected = src_ptr[i]; 
+        uint8_t expected = src_ptr[i];
         uint8_t actual   = dst_ptr[i];
 
         if (expected != actual) {
@@ -64,6 +64,18 @@ int test_idma_1D (int core_id, uint32_t size, int ext2loc, int loc2loc) {
                 PRINTF ("Error: expected @%8x = %8x vs actual @%8x = %8x \n", expected, &src_ptr[i], actual, &dst_ptr[i]);
             }
         }
+    }
+
+    if(core_id == 0) {
+        if(error == 0){
+        printf("TEST PASSED!\n");
+        } else {
+        printf("TEST FAILED!\n");
+        }
+
+        printf("Writing to mailbox...\n");
+        pulp_write32(0x10404008, error);
+        pulp_write32(0x10404020, 0x1);
     }
 
     return error;
@@ -222,6 +234,11 @@ int cluster_task () {
 
 int main () {
     int retval = 1;
+
+    if (rt_core_id() == 0) {
+        printf("TEST IDMA MULTI CORE 1D - start!\n");
+    }
+
     #ifdef ARCHI_HAS_FC
     PRINTF ("Fabric Controller calling cluster task \n");
     if (rt_cluster_id() != 0)
--- parallel_bare_tests/parMatrixMul16/matrixMul.c
+++ parallel_bare_tests/parMatrixMul16/matrixMul.c
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-/* 
+/*
  * Mantainer: Luca Valente, luca.valente2@unibo.it
  */
 
@@ -35,6 +35,10 @@ unsigned int num_cores;
 
 int main()
 {
+  if (rt_core_id() == 0) {
+    printf("TEST PAR MATRIX MUL 16 - start!\n");
+  }
+
   if (rt_cluster_id() != 0)
     return bench_cluster_forward(0);
 
@@ -79,7 +83,7 @@ void check_matrix_mul(testresult_t *result, void (*start)(), void (*stop)()) {
   for(i = lb; i < ub; i++) {
     for(j = 0; j < SIZE; j++) {
       g_mC[i][j] = 0;
-  
+
       for(k = 0; k < SIZE; k++) {
         g_mC[i][j] += g_mA[i][k] * g_mB[k][j];
       }
@@ -92,6 +96,16 @@ void check_matrix_mul(testresult_t *result, void (*start)(), void (*stop)()) {
 
   if(core_id == 0) {
     result->errors = matrix_check();
+
+    if(result->errors == 0){
+      printf("TEST PASSED!\n");
+    } else {
+      printf("TEST FAILED!\n");
+    }
+
+    printf("Writing to mailbox...\n");
+    pulp_write32(0x10404008, result->errors);
+    pulp_write32(0x10404020, 0x1);
   }
 }
 
