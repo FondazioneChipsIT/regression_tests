9b4d8bd1e7e5565847f718f1ca8b2e166a9b3fd2
rgandolfi <riccardogandi95@gmail.com>
2026-01-20 11:16:09 +0100

    Fixed Deeploy non-tiled adder test

--- opentitan-cluster/deeploy/Adder/Adder_untiled/test.c
+++ opentitan-cluster/deeploy/Adder/Adder_untiled/test.c
@@ -1,22 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
+#include <stdint.h>
 #include "pulp.h"
-#include "stdint.h"
-// #include "pulp_nn_kernels.h"
-// #include "DeeployBasicMath.h"
-// #include "DeeployPULPMath.h"
-// #include "bsp/ram.h"
-// #include "pulp_core.h"
 #include "Network.h"
 
 int main() {
     uint32_t tot_err, tot_tested;
     tot_err = 0;
     tot_tested = 0;
+
     if (rt_core_id() == 0) {
+        printf ("Core number is %d \n", get_core_num());
         printf ("Starting Deeploy test \n");
         printf ("Before Initnetwork \n");
+        DeeployNetwork_input_0 = (int8_t *)pi_l2_malloc(sizeof(int8_t) * 125);
+        DeeployNetwork_input_1 = (int8_t *)pi_l2_malloc(sizeof(int8_t) * 125);
+        DeeployNetwork_output_0 = (int32_t *)pi_l2_malloc(sizeof(int32_t) * 125);
         InitNetwork();
         printf ("After Initnetwork \n");
         for (uint32_t buf = 0; buf < DeeployNetwork_num_inputs; buf++) {
@@ -33,14 +33,15 @@ int main() {
         for (uint32_t buf = 0; buf < DeeployNetwork_num_outputs; buf++) {
             tot_tested += DeeployNetwork_outputs_bytes[buf] / sizeof(int32_t);
         }
-
+        tot_err = tot_tested;
+        printf ("Tot err is %d \n", tot_err);
         for (uint32_t buf = 0; buf < DeeployNetwork_num_outputs; buf++) {
             tot_tested += DeeployNetwork_outputs_bytes[buf] / sizeof(int32_t);
 
             if ((uint32_t)DeeployNetwork_outputs[buf] < 0x1000000) {
                 compbuf = pi_l2_malloc((int)DeeployNetwork_outputs_bytes[buf]);
                 // ram_read(compbuf, DeeployNetwork_outputs[buf], DeeployNetwork_outputs_bytes[buf]);
-                plp_cl_dma_wait_toL2(plp_cl_dma_l1ToExt(&compbuf, &DeeployNetwork_outputs[buf], DeeployNetwork_outputs_bytes[buf]));
+                plp_cl_dma_wait_toL2(plp_cl_dma_l1ToExt((unsigned int) compbuf, (unsigned int) DeeployNetwork_outputs[buf], DeeployNetwork_outputs_bytes[buf]));
             } else {
                 compbuf = DeeployNetwork_outputs[buf];
             }
@@ -53,20 +54,30 @@ int main() {
 
                 if (diff) {
                     printf ("Difference found \n");
-                    tot_err++;
+                } else {
+                    tot_err--;
                 }
             }
             if ((uint32_t)DeeployNetwork_outputs[buf] < 0x1000000) {
                 pi_l2_free(compbuf, (int)DeeployNetwork_outputs_bytes[buf]);
             }
+
+            // Free the previously allocated memory regions
+            
+            pi_l2_free(DeeployNetwork_input_0, sizeof(int8_t) * 125);
+            pi_l2_free(DeeployNetwork_input_1, sizeof(int8_t) * 125);
+            pi_l2_free(DeeployNetwork_output_0, sizeof(int8_t) * 125);
         }
+
+        if (tot_err == 0) {
+            printf ("TEST PASSED!!! \n");
+        } else {
+            printf ("TEST FAILED!!! \n");
+        }
+        printf("Writing %d errors to mailbox...\n", tot_err);
+        pulp_write32(0x10404008, tot_err);
+        pulp_write32(0x10404020, 0x1);
     }
-
-    synch_barrier();
-
-    printf("Writing to mailbox...\n");
-    pulp_write32(0x10404008, (tot_err));
-    pulp_write32(0x10404020, 0x1);
 
     synch_barrier();
     return 0;
