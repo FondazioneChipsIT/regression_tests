8f246cc3f6ab8a7356b757b05366e4a3c696856f
Gabriele Lodi <gabriele.lodi93@gmail.com>
2026-03-04 18:42:17 +0100

    [MBOX] Remove NO_STANDALONE define

--- opentitan-cluster/mbox_test/test.c
+++ opentitan-cluster/mbox_test/test.c
@@ -27,16 +27,8 @@ int main() {
 
   if(core_id() == 0){
     printf("Writing to mailbox...\n");
-#ifdef NO_STANDALONE
     hal_write_to_mailbox(ARCHI_MAILBOX_LETTER0_OFFSET, 0xBAADC0DE);
     hal_write_to_mailbox(ARCHI_MAILBOX_LETTER1_OFFSET, 0xBAADC0DE);
-#else
-    hal_write_to_mailbox(ARCHI_MAILBOXES_REG0_OFFSET, 0xBAADC0DE);
-    hal_write_to_mailbox(ARCHI_MAILBOXES_REG1_OFFSET, 0xBAADC0DE);
-    hal_write_to_mailbox(ARCHI_MAILBOXES_REG2_OFFSET, 0xBAADC0DE);
-    hal_write_to_mailbox(ARCHI_MAILBOXES_REG3_OFFSET, 0xBAADC0DE);
-    hal_write_to_mailbox(ARCHI_MAILBOXES_REG4_OFFSET, 0xBAADC0DE);
-#endif
   }
 
   return 0;
